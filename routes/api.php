a40f2f914f51fe0c62713a25b18a2268a247869d
mohamadir <mohamdib@gmail.com>
2018-07-08 13:48:04 +0300

    push

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/mohamed',function(){
+
+    return 'mohamed welcome ';
+
+
+});
